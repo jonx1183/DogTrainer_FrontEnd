486576ccf6a182753c8a23d0bdad6297fdfb42e6
jonx1183 <jonx1183@stud.kea.dk>
2023-05-04 11:51:32 +0200

    Working on Homepage.js

---
+++ Js/Homepage.js
@@ -0,0 +1,34 @@
+const urlGetPosts = 'http://localhost:8080/hej'
+const tablePost = document.getElementById('post-list')
+
+async function createPostTable(post) {
+    const row = document.createElement('tr');
+    row.innerHTML = `
+      <td>${post.id}</td>
+      <td>${post.title}</td>
+      <td>${post.body}</td>
+    `;
+
+    cell = row.insertCell(4)
+    let pbUpdate = document.createElement("button")
+    pbUpdate.textContent = "Opdater"
+    pbUpdate.className = "buttonupdate"
+    pbUpdate.addEventListener('click', function () {
+        const posid = post.postId
+        printPost(posid, post)
+    })
+    cell.appendChild(pbUpdate)
+    tablePost.appendChild(row);
+}
+
+let lstPosts = []
+async function actionFetchPosts() {
+    lstPosts = await fetchAny(urlGetPosts);
+    tablePost.innerHTML = '';
+    lstPosts.forEach(createPostTable)
+}
+
+function printPost(posid, post) {
+    console.log(posid)
+    console.log(post)
+}
